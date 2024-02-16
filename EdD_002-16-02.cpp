4e418c6a859ddfbc615f11a376ae7ec76ec3d2cc
unknown <miguelanperibeiro@gmail.com>
2024-02-16 10:53:29 -0300

    16-02

---
+++ EdD_002-16-02.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+main(){
+    float n1, n2;
+    cout << "informe 2 numeros reais \n";
+    cin >> n1 >> n2;
+    cout << "media" << (n1+n2)/2 << endl;
+    system("pause");
+}
