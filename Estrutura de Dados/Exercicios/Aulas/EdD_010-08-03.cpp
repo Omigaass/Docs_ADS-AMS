c707a72b1f9220be3e9166b77459feb6beeef156
Omigaass <131165278+Omigaass@users.noreply.github.com>
2024-03-08 10:12:29 -0300

    Update EdD_010-08-03.cpp

--- Estrutura de Dados/Exercicios/Aulas/EdD_010-08-03.cpp
+++ Estrutura de Dados/Exercicios/Aulas/EdD_010-08-03.cpp
@@ -1,61 +1,64 @@
 #include <iostream>
 using namespace std;
-int v[100];
-int n=0;
 
 class Vetor {
-    public: void inserção(int valor){    
-        v[n] = valor;
+    private: int v[10], n;
+    public: vetor(){
+        n = 0;
+    }
+
+    public: void exibicao(){
+        cout << "\n Mostrando todos os elementos do vetor: \n";
+        for(int i = 0; i < n; i++) cout << " " << v[i];
+        cout << endl;
+    }
+    public: void remocao() {
+        cout << "Forneca valor para remoção: ";
+        int i, chave;
+        cin >> chave;
+        for(i=0;i<n;i++){
+            if(v[i] == chave){
+                if(i != (n-1)){
+                    for(int k = i; k < (k-1); k++){
+                        v[k] = v[k + 1];
+                    }
+                    n--;
+                    break;
+                }
+            }
+        }
+    }
+    public: void inserção(int chave){    
+        v[n] = chave;
         n++;    
+    }
+    public: int pesquisa(){
+        cout << "Forneca valor para busca: ";
+        int chave;
+        cin >> chave;
+        for ( int = 0; i < n; i++){
+             if(v[i] == chave) return i;
+        }
+        return -1;
     }
 };
 
 int main() {
- 
-        // Inserindo valores no vetor
-        cout << "Informe 10 valores inteiros para o vetor" << endl;
-        int i;
-        for(i=0; i<10; i++) {
-                cin >> v[i];
-                n++;
-        }
- 
-        // Mostrando valores no vetor
-        cout << "\nMostrando todos elementos do vetor" << endl;
-        for(i=0; i<n; i++) {
-                cout << " " << v[i];
-        }
-        cout << endl;
- 
-        // Buscando valores no vetor
-        cout << "\nForneca um valor para busca: ";
-        int chave;
+    vetor v;
+    cout << "Forneca 5 valores inteiro: ";
+    int chave;
+    for(int i =0; i < 5; i++){
         cin >> chave;
-        for (i=0; i<n; i++) {
-                if ( v[i] == chave ) { break; }
-        }
-        if (i == n) { cout << "Chave NAO encontrada" << endl; }
-        else { cout << "Chave encontrada no indice " << i << endl; }
- 
-        // Removendo valores no vetor
-        cout << "\nForneca um valor para remocao: ";
-        cin >> chave;
-        for (i=0; i<n ;i++) {
-                if (v[i] == chave) {
-                        if (i != (n-1)) {
-                                for(int k=i; k<(n-1); k++)  v[k]=v[k+1];
-                        }
-                n--;
-                break;
-                }
-        }                                                
- 
-        // Mostrando valores no vetor
-        cout << "\nMostrando todos elementos do vetor" << endl;
-        for(i=0; i<n; i++) {
-                cout << " " << v[i];
-        }
-        cout << endl;
- 
-        return 0;
+        v.insercao(chave);
+    }
+    v.exibicao();
+    int retorno;
+    retorno = v.pesquisa();
+    if( retorno == -1)
+        cout << "Valor nao encontrado\n";
+    else
+        cout << "Valor encontrado na posicao " << retorno << endl;
+    v.remocao();
+    v.exibicao();
+    return 0;
 }
